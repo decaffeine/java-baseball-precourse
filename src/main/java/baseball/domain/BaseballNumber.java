239687f31e36620ce4f84600dbe0ba6b45c1761d
decaffeine <decaffeine12@gmail.com>
2022-04-13 23:01:40 +0900

    feat: 야구 게임에서 사용할 3자리 숫자 도메인 객체와 테스트 구현

---
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class BaseballNumber {
+
+    private static final int MAX_DIGIT = 3;
+    private static final Pattern pattern = Pattern.compile("^[1-9]*$");
+    private final List<Integer> numbers;
+
+    BaseballNumber(String line) {
+        validateInput(line);
+        int input = Integer.parseInt(line);
+
+        numbers = new ArrayList<>(MAX_DIGIT);
+        numbers.add(input / 100);
+        numbers.add(input % 100 / 10);
+        numbers.add(input % 10);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateInput(String line) throws IllegalArgumentException {
+        if (!pattern.matcher(line).find()) {
+            throw new IllegalArgumentException("입력은 숫자여야 합니다.");
+        }
+        if (line.length() != MAX_DIGIT) {
+            throw new IllegalArgumentException("숫자는 3자리여야 합니다.");
+        }
+    }
+
+
+}
---
+++ src/test/java/baseball/domain/BaseballNumberTest.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BaseballNumberTest {
+
+    @Test
+    void 더_많은_자릿수의_숫자() {
+        assertThatThrownBy(() -> new BaseballNumber("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 더_적은_자릿수의_숫자() {
+        assertThatThrownBy(() -> new BaseballNumber("1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_아닌_입력() {
+        assertThatThrownBy(() -> new BaseballNumber("가나1다"))
+                .hasMessage("입력은 숫자여야 합니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력이_객체로_변환됨() {
+        assertThat(new BaseballNumber("537")
+                .getNumbers()).containsExactly(5, 3, 7);
+
+    }
+}
