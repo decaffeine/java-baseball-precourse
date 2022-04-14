44ee25fed1e21b641f18d2ba4a004104cc753c44
decaffeine <decaffeine12@gmail.com>
2022-04-14 20:01:14 +0900

    feat: 숫자 비교하여 결과 생성하는 로직 구현

--- src/main/java/baseball/domain/BaseballScore.java
+++ src/main/java/baseball/domain/BaseballScore.java
@@ -1,17 +1,42 @@
 package baseball.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class BaseballScore {
+
+    private BaseballNumber answer;
     private int ball;
     private int strike;
 
     BaseballScore(BaseballNumber answer, BaseballNumber guess) {
-        if (answer.getNumbers().containsAll(guess.getNumbers())) {
-            this.strike = 3;
+        this.answer = answer;
+        for (int i = 0; i < guess.getNumbers().size(); i++) {
+            ball += scoreBall(guess, i);
+            strike += scoreStrike(guess, i);
         }
-
     }
+
+    private int scoreBall(BaseballNumber guess, int location) {
+        int result = 0;
+        for (int j = 0; j < answer.getNumbers().size(); j++) {
+            result +=
+                    (j != location) && Objects.equals(answer.getNumbers().get(j), guess.getNumbers().get(location)) ? 1
+                            : 0;
+        }
+        return result;
+    }
+
+    private int scoreStrike(BaseballNumber guess, int location) {
+        int result = 0;
+        for (int j = 0; j < guess.getNumbers().size(); j++) {
+            result +=
+                    (j == location) && Objects.equals(answer.getNumbers().get(j), guess.getNumbers().get(location)) ? 1
+                            : 0;
+        }
+        return result;
+    }
+
 
     public int getBall() {
         return ball;
--- src/test/java/baseball/domain/BaseballScoreTest.java
+++ src/test/java/baseball/domain/BaseballScoreTest.java
@@ -2,9 +2,6 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BaseballScoreTest {
@@ -19,7 +16,6 @@ public class BaseballScoreTest {
 
     @Test
     void 게임_진행_1볼_1스트라이크() {
-        // TODO 여기서부터
         BaseballNumber computer = new BaseballNumber("245");
         BaseballNumber user = new BaseballNumber("541");
         BaseballScore score = new BaseballScore(computer, user);
